aa0f125b3bea52504474e7ff7d7e14387ba72c4b
Dmitry Taran <dmit.taran2018@gmail.com>
2022-11-09 09:56:11 +0200

    Base project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Education from './pages/Education';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/Education" element={<Education />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
